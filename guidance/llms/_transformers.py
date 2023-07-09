20d43817629dac85bfe84440bb1469e3b0d078e6
Enrique Shockwave <33002121+qeternity@users.noreply.github.com>
2023-07-09 15:55:33 +0100

    .

--- guidance/llms/_transformers.py
+++ guidance/llms/_transformers.py
@@ -669,6 +669,8 @@ class TransformersStreamer():
         # extract the scores if we are given them (and format them to be the same shape as the tokens)
         if self.logprobs:
             assert len(new_tokens) == 1, "logprobs are not supported for batched generation right now in guidance.llms.Transformers"
+            print(token_obj['scores'])
+            raise Exception('stop')
             new_scores = [torch.nn.functional.log_softmax(x, dim=-1).cpu() for x in token_obj['scores']]
             len_diff = len(new_tokens[0]) - len(new_scores)
             if len_diff > 0:
