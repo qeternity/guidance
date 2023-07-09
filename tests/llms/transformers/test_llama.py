1c2357db745e2dcdb2e430192df001c8ddeb2298
Enrique Shockwave <33002121+qeternity@users.noreply.github.com>
2023-07-09 13:01:24 +0100

    .

--- tests/llms/transformers/test_llama.py
+++ tests/llms/transformers/test_llama.py
@@ -11,6 +11,9 @@ def test_basic():
         pytest.skip("No GPU, so skipping large model test.")
 
     # just make sure it runs
-    llm = guidance.llms.transformers.LLaMA('../../models/llama/7B', device=1)
+    llm = guidance.llms.transformers.LLaMA('elinas/llama-13b-hf-transformers-4.29', device=1)
     out = guidance("""The height of the Sears tower is {{gen 'answer' max_tokens=10}}""", llm=llm)()
+    out = guidance(
+        """The Sun is very {{#select 'answer'}}hot{{or}}cold{{/select}}.""", llm=llm
+    )()
     assert len(out["answer"]) > 0
