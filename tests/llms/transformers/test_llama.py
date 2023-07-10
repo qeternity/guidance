76527d26fc5e31c9cdef986dc43e9654a482dde7
Chase Bennett <33002121+qeternity@users.noreply.github.com>
2023-07-10 15:54:21 +0100

    .

--- tests/llms/transformers/test_llama.py
+++ tests/llms/transformers/test_llama.py
@@ -23,7 +23,7 @@ def test_basic():
     )
 
     # just make sure it runs
-    guidance.llm = guidance.llms.transformers.LLaMA(model, tokenizer, device='cuda', torch_dtype=torch.float16, caching=False)
+    guidance.llm = guidance.llms.transformers.LLaMA(model, tokenizer, torch_dtype=torch.float16, caching=False)
 
     out = guidance("""
 The following are requests for fuel pricing for maritime vessels as conversations.
