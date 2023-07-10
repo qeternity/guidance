8b90618afec5bd769e3c89b3a8253560e9c333af
Chase Bennett <33002121+qeternity@users.noreply.github.com>
2023-07-10 15:02:53 +0100

    .

--- guidance/llms/_exllama.py
+++ guidance/llms/_exllama.py
@@ -350,7 +350,7 @@ class ExLLaMASession(LLMSession):
                         print(token)
                         self.llm.tokenizer.decode(token)
                         print(biased_token)
-                        self.llm.tokenizer_hf.decode(token)
+                        self.llm.tokenizer_hf.decode(token[0])
                         print('#'*50)
                     # _seq = self.llm.model_obj.sequence[:, :-1]
                     # _seq = torch.cat((_seq, biased_token), dim = 1)
