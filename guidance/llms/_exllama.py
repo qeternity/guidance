9e0d1e244cb41d25ae11758df52dc4280b30c971
Chase Bennett <33002121+qeternity@users.noreply.github.com>
2023-07-10 11:25:19 +0100

    .

--- guidance/llms/_exllama.py
+++ guidance/llms/_exllama.py
@@ -579,6 +579,8 @@ class RegexLogitsProcessor():
         # bias allowed tokens
         min_to_bias = float(scores[0, to_bias].min())
         bias_value = scores[0, sort_inds[0, 0]] - min_to_bias + 10 # make sure the tokens that fit the pattern have higher scores than the top value
+        print(bias_value)
+        raise Exception('stop')
         for x in to_bias:
             self.bias_vector[x] = bias_value
         out = scores + self.bias_vector.to(scores.device)
